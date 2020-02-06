9e5d2851b51df89d7e67603fc2b9f2638e13ed0a
Marcel Papp <marcel.papp@inveda.net>
2020-02-06 11:36:15 +0100

    HTMLEntitiy ArrayAccess

--- oti/model/HTMLElements.php
+++ oti/model/HTMLElements.php
@@ -1,5 +1,43 @@
 <?php
 
-class HTMLElements  {
+class HTMLElements implements ArrayAccess {
 
+    private $html_elements;
+
+    public function __construct ()
+    {
+        $this -> html_elements;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function offsetExists ( $offset )
+    {
+        // TODO: Implement offsetExists() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function offsetGet ( $offset )
+    {
+        // TODO: Implement offsetGet() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function offsetSet ( $offset , $value )
+    {
+        // TODO: Implement offsetSet() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function offsetUnset ( $offset )
+    {
+        // TODO: Implement offsetUnset() method.
+    }
 }
